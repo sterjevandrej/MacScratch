e9de8625f93ecad68777073f819a4e7057d3c40d
Bojan Ognenov <ognenovbojan@gmail.com>
2023-12-19 21:48:16 +0100

    'login_and_logout_done'

--- Homework 3/dians_proba/dians_proba/src/main/java/com/example/dians_proba/web/LogoutController.java
+++ Homework 3/dians_proba/dians_proba/src/main/java/com/example/dians_proba/web/LogoutController.java
@@ -3,10 +3,10 @@ package com.example.dians_proba.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
-@RequestMapping("/logout") //TO DO: IMPLEMENT ACTION="/logout" SOMEWHERE in HTML
+@RequestMapping("/logout")
 public class LogoutController {
     @GetMapping
     public String logout(HttpServletRequest request) {
