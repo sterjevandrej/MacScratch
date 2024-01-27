c1b97157f3d28dfab6249ce8fd946b0edb4c9050
Bojan Ognenov <ognenovbojan@gmail.com>
2024-01-27 14:29:43 +0100

    added postMapping

--- Homework 3/ListsMicroservice/src/main/java/spring/listsmicroservice/web/Controller.java
+++ Homework 3/ListsMicroservice/src/main/java/spring/listsmicroservice/web/Controller.java
@@ -1,14 +1,12 @@
 package spring.listsmicroservice.web;
 
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import spring.listsmicroservice.model.Monument;
 import spring.listsmicroservice.model.User;
 import spring.listsmicroservice.service.FavoritesService;
+import spring.listsmicroservice.service.MonumentService;
 import spring.listsmicroservice.service.VisitedService;
 import spring.listsmicroservice.service.WishService;
 
@@ -20,11 +18,13 @@ public class Controller {
     private final WishService wishService;
     private final VisitedService visitedService;
     private final FavoritesService favoritesService;
+    private final MonumentService monumentService;
 
-    public Controller(WishService wishService, VisitedService visitedService, FavoritesService favoritesService) {
+    public Controller(WishService wishService, VisitedService visitedService, FavoritesService favoritesService, MonumentService monumentService) {
         this.wishService = wishService;
         this.visitedService = visitedService;
         this.favoritesService = favoritesService;
+        this.monumentService = monumentService;
     }
 
     /*Returns all wishlist monuments for the given user if the user is logged in */
@@ -41,7 +41,7 @@ public class Controller {
     /*Returns all visited monuments for the given user if the user is logged in */
 
     @GetMapping("/visited")
-    public List<Monument> visited( Model model, HttpServletRequest request) {
+    public List<Monument> visited(HttpServletRequest request) {
         User user = (User) request.getSession().getAttribute("user");
         if (user != null)
             return visitedService.getVisitedList(user.getUsername());
@@ -52,11 +52,49 @@ public class Controller {
     /*Returns all favourite monuments for the given user if the user is logged in */
 
     @GetMapping("/favourites")
-    public List<Monument> favourites(Model model, HttpServletRequest request) {
+    public List<Monument> favourites(HttpServletRequest request) {
         User user = (User) request.getSession().getAttribute("user");
         if (user != null)
             return favoritesService.getFavoritesList(user.getUsername());
         else
             return null;
+    }
+
+    @PostMapping("/wishlist/{name}")
+    public ResponseEntity<String> addToWishList(@PathVariable String name, HttpServletRequest request) {
+        User user = (User) request.getSession().getAttribute("user");
+        if (user != null) {
+            wishService.addToWishList(user.getUsername(), name);
+            return ResponseEntity.ok("addedToWish");
+        } else {
+            return ResponseEntity.status(401).body("Unauthorized");
+        }
+    }
+
+    @PostMapping("/visited/{name}")
+    public ResponseEntity<String> addToVisited(@PathVariable String name, HttpServletRequest request) {
+        User user = (User) request.getSession().getAttribute("user");
+        if (user != null) {
+            visitedService.addToVisitedList(user.getUsername(), name);
+            return ResponseEntity.ok("addedToVisited");
+        } else {
+            return ResponseEntity.status(401).body("Unauthorized");
+        }
+    }
+
+    @PostMapping("/favorites/{name}")
+    public ResponseEntity<String> addToFavorites(@PathVariable String name, HttpServletRequest request) {
+        User user = (User) request.getSession().getAttribute("user");
+        if (user != null) {
+            favoritesService.addToFavoritesList(user.getUsername(), name);
+            return ResponseEntity.ok("addedToFavorites");
+        } else {
+            return ResponseEntity.status(401).body("Unauthorized");
+        }
+    }
+
+    @GetMapping("/all")
+    public List<Monument> listPlaces() {
+        return monumentService.listAllMonuments();
     }
 }
